1df98cb9b0bfa226aa58f141bb8905108a5792f0
ppppbamzy <pppp.bamzy@gmail.com>
2022-03-15 17:13:32 +0800

    sample 1

--- plonk/examples/sample_usage_1.rs
+++ plonk/examples/sample_usage_1.rs
@@ -1,50 +1,52 @@
-// Copyright (c) 2022 Espresso Systems (espressosys.com)
-// This file is part of the Jellyfish library.
-
-// You should have received a copy of the MIT License
-// along with the Jellyfish library. If not, see <https://mit-license.org/>.
-
-//! This file contains an example showing how to build a proof of knowledge
-//! of the exponent over a native field.
-//!
-//! - secret input `x`;
-//! - public generator `G`;
-//! - public group element `X := xG`
-
 use ark_bls12_381::{Bls12_381, Fr};
-use ark_ec::{
-    twisted_edwards_extended::GroupAffine as TEAffine, AffineCurve, ModelParameters, PairingEngine,
-    ProjectiveCurve, TEModelParameters,
-};
-// use ark_ed_on_bls12_381::{EdwardsAffine, EdwardsParameters, Fr};
-use ark_ff::PrimeField;
-use ark_std::{rand::SeedableRng, UniformRand};
+use ark_std::{rand::SeedableRng};
 use jf_plonk::{
-    circuit::{customized::ecc::Point, Arithmetization, Circuit, PlonkCircuit},
+    circuit::{Arithmetization, Circuit, PlonkCircuit},
     errors::PlonkError,
     proof_system::{PlonkKzgSnark, Snark},
     transcript::StandardTranscript,
 };
-use jf_utils::fr_to_fq;
 use rand_chacha::ChaCha20Rng;
 
-// The following example proves knowledge of exponent.
 #[allow(non_snake_case)]
 fn main() -> Result<(), PlonkError> {
+    ///////////////////////////////
+    // 电路部分
+    ///////////////////////////////
+
+    // 私有值
     let x = Fr::from(13_u64);
     let y = Fr::from(17_u64);
 
+    // 新建一个电路
     let mut circuit: PlonkCircuit<Fr> = PlonkCircuit::new_turbo_plonk();
+
+    // 加入私有变量并绑定值
+    // verifier 在此处使用 dummy value
     let x_var = circuit.create_variable(x)?;
     let y_var = circuit.create_variable(y)?;
+
+    // 加入中间变量 z = x + y，其值自动算出并保存在变量中
     let z_var = circuit.add(x_var, y_var)?;
+    // 可以检查一下 z 的值
+    assert_eq!(Fr::from(30_u64), circuit.witness(z_var)?);
+
+    // 加入常量 2
     let two_var = circuit.create_constant_variable(Fr::from(2_u64))?;
+
+    // 加入结果变量 w = 2 * z，其值自动算出并保存在变量中
     let w_var = circuit.mul(z_var, two_var)?;
-    circuit.set_variable_public(w_var)?;
+    // 可以检查一下 w 的值
     assert_eq!(Fr::from(60_u64), circuit.witness(w_var)?);
+    // 设 w 为公开变量
+    circuit.set_variable_public(w_var)?;
 
     // 固定的后处理
     circuit.finalize_for_arithmetization()?;
+
+    ///////////////////////////////
+    // 协议部分
+    ///////////////////////////////
 
     let mut rng = ChaCha20Rng::from_seed([0u8; 32]);
     let srs_size = circuit.srs_size()?;
@@ -52,12 +54,14 @@ fn main() -> Result<(), PlonkError> {
 
     let (pk, vk) = PlonkKzgSnark::<Bls12_381>::preprocess(&srs, &circuit)?;
 
+    // prover 生成 proof
+    // (verifier 跳过这步)
     let proof = PlonkKzgSnark::<Bls12_381>::prove::<_, _, StandardTranscript>(
         &mut rng, &circuit, &pk, None,
     )?;
 
+    // verifier 验证 proof
     let public_inputs = circuit.public_input().unwrap();
-    
     let extra_transcript_init_msg = None;
     assert!(PlonkKzgSnark::<Bls12_381>::verify::<StandardTranscript>(
         &vk,
